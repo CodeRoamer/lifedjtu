cd240d8e691a6dda279b7a606eeaa508377f31a7
lihe911119 <476282190@qq.com>
2013-12-21 15:15:45 +0800

    remoteservice接口

---
+++ 0/src/com/lifedjtu/jw/business/JWRemoteService.java
@@ -0,0 +1,28 @@
+package com.lifedjtu.jw.business;
+
+
+import java.util.List;
+
+import com.lifedjtu.jw.pojos.*;
+import com.lifedjtu.jw.pojos.dto.BuildingDto;
+import com.lifedjtu.jw.pojos.dto.RoomDto;
+import com.lifedjtu.jw.pojos.dto.ScoreDto;
+import com.lifedjtu.jw.pojos.dto.StudentRegistry;
+
+public interface JWRemoteService {
+	public String signinRemote(String studentId, String password);
+	
+	public StudentRegistry fetchStudentRegistry(String sessionId);
+	
+	public boolean changeRemotePassword(String sessionId, String originPass, String newPass, String newPassAgain);
+	
+	public BuildingDto queryBuildingOnDate(String sessionId, int aid, int buildingId, int week, int weekday);
+	
+	public RoomDto queryRoom(String sessionId, int aid, int buildingId, int roomId);
+	
+	public List<Course> queryRemoteCourseTable(String sessionId);
+	
+	public List<Exam> queryRemoteExams(String sessionId);
+	
+	public List<ScoreDto> queryRemoteScores(String session);
+}
